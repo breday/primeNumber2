faae071e5267825e63a2088e7b5034e002f57de6
brenda nyokabi <brndnyokabi@gmail.com>
2017-03-06 16:36:00 +0300

    prime numbers file

---
+++ primeNumber.py
@@ -0,0 +1,23 @@
+def primeNum(number):
+    
+#0 and 1 are not prime numbers 
+    if number < 2:
+        print (number,"not considered a Prime number")
+
+#number divisible by two
+    elif number == 2:
+        print (number,"is Prime number") 
+
+#checks if the mod is zero to determine if items in range(2,x) are prime numbers 
+    for n in range(2, number):
+        if number % n == 0:
+            return (number,"is not a Prime number")
+
+    for n in range(2,number):
+	    if (number % 2 == 0) or (number % 5 == 0):
+	        return (n," is a Prime Number")
+	        break
+	
+
+#returnresults
+print(primeNumber(28))
---
+++ test_primeNumber.py
@@ -0,0 +1,30 @@
+import unittest
+from primeNumber import primeNumber
+
+
+class primeTestCase(unittest.TestCase):
+    """Testing orimeNumber.py`."""
+
+    def test_primeNum_odd_prime(self):
+        """test if 7 is a prime number"""
+        self.assertTrue(primeNum(7))
+
+    def test_primeNum_even_on_prime(self):
+        """test if 28 is not a prime number"""
+        self.assertFalse(primeNum(28), msg='28 is not prime!')
+
+    def test_primeNum_eight_non_prime(self):
+        """test if 8 is not a prime number"""
+        self.assertFalse(primeNum(8), msg='Eight is not prime!')
+
+    def test_primeNum_zero_non_prime(self):
+        """test if zero is not a prime number"""
+        self.assertFalse(primeNum(0), msg='zero is not prime!')
+
+    def test_negative_number(self):
+        """test if -tive numbers are not  prime"""
+        for index in range(-1, -10, -1):
+            self.assertFalse(primeNum(index))
+
+if __name__ == '__main__':
+unittest.main()
